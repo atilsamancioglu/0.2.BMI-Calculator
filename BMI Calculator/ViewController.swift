b9c3e39b1f55500b7427398dbae48e2830c086be
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2016-12-04 19:13:48 +0300

    rounding fixed

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightText: UITextField!
+    @IBOutlet weak var heightText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    var userWeight = 0.00
+    var userHeight = 0.00
+    var userHeightSquare = 0.00
+    var finalBMI = 0.00
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func calculateButtonClicked(_ sender: Any) {
+        
+        if let newWeight = Double(weightText.text!) {
+            userWeight = newWeight * 0.45
+        }
+        
+        if let newHeight = Double(heightText.text!) {
+            userHeight = newHeight * 0.025
+        }
+        
+        userHeightSquare = userHeight * userHeight
+        
+        finalBMI = userWeight / userHeightSquare
+        
+        resultLabel.text = "BMI Result: \(finalBMI.rounded())"
+        
+        if finalBMI < 19 {
+            commentLabel.text = "You need to gain some weight!"
+        } else if finalBMI > 19 && finalBMI < 25 {
+            commentLabel.text = "You are healthy, congratulations!"
+        } else {
+            commentLabel.text = "You need to loose some weight!"
+        }
+    
+    }
 
 }
 
